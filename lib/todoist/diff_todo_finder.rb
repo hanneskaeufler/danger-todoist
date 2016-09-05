d4322be1fc604df51fe64157b09c52bc819f3f65
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-09-05 21:56:02 +0200

    remove testing comments

--- lib/todoist/diff_todo_finder.rb
+++ lib/todoist/diff_todo_finder.rb
@@ -23,10 +23,7 @@ module Danger
       todos
     end
 
-    # TODO: I want to see this
     class Todo < Struct.new(:file, :text)
     end
-
-    # this however, is not supposed to be a todo
   end
 end
