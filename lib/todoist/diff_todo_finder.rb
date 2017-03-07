5f690de2f68fb9f6c55a5ca99f45c67a1bcf99cd
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-03-07 21:23:13 +0100

    clean up multiline implementation

--- lib/todoist/diff_todo_finder.rb
+++ lib/todoist/diff_todo_finder.rb
@@ -13,19 +13,9 @@ module Danger
       diffs.each do |diff|
         matches = diff.patch.scan(regexp)
         next if matches.empty?
-        # require "pry"
-        # binding.pry
 
         matches.each do |match|
-          comment_indicator = match[0]
-          entire_todo = match[2]
-          rest = match[4]
-          final_todo = entire_todo
-            .gsub(rest || "", "")
-            .gsub(comment_indicator, "")
-            .gsub(/\n/, "")
-            .strip
-          todos << Danger::Todo.new(diff.path, final_todo)
+          todos << Danger::Todo.new(diff.path, clean_todo_text(match))
         end
       end
       todos
@@ -33,18 +23,22 @@ module Danger
 
     private
 
+    def clean_todo_text(match)
+      comment_indicator, _, entire_todo, _, rest = match
+      entire_todo.gsub(rest || "", "")
+                 .gsub(comment_indicator, "")
+                 .delete("\n")
+                 .strip
+    end
+
     def todo_regexp
-      /(?<comment_indicator>^\+\s*[^a-z0-9\+\s]+)(\n\+)?\s+(?<todo_indicator>#{@keywords.join("|")})[\s:]{1}(?<entire_text>(?<text>[^\n]*)(?<rest>\n\k<comment_indicator>\s*[^\n]*)*)/ixm
-      # /
-      # (?<comment_indicator>^\+        # we only look at additions, marked by + in diffs
-      # \s*                 # followed by optional space
-      # [^a-z0-9\+\s]+)    # anything looking like a comment indicator
-      # (\n\+)?             # allow multiline comment markers
-      # \s+                 # followed by at least one space
-      # (?<todo_indicator>#{@keywords.join("|")})       # our todo indicator
-      # [\s:]{1}            # followed by a space or colon
-      # (?<text>.*)(\n\<comment_indicator>\s+.*|$)*        # matching any text until the end of the line
-    # /ix
+      /
+      (?<comment_indicator>^\+\s*[^a-z0-9\+\s]+)
+      (\n\+)?\s+
+      (?<todo_indicator>#{@keywords.join("|")})[\s:]{1}
+      (?<entire_text>(?<text>[^\n]*)
+      (?<rest>\n\k<comment_indicator>\s*[^\n]*)*)
+      /ixm
     end
   end
 end
